4b08bb6ed0001efe1b8a2d827873cafa5c0539b8
Felix <felixtjung@gmail.com>
2020-11-17 20:09:49 +1100

    Add implementation of AddBattleShip.

--- BattleshipGame/Scripts/BattleshipBoard.ts
+++ BattleshipGame/Scripts/BattleshipBoard.ts
@@ -1,14 +1,74 @@
-﻿class BattleshipBoard {
-    battleshipBoard: number[][];
+﻿export class BattleshipBoard {
+    private readonly _boardDimension: number;
+    private readonly _battleshipBoard: number[][];
+    private readonly _destroyedBattleshipIds: number[];
+    private _newBattleshipIdCounter = 1;
 
     constructor(boardDimension: number) {
-        this.battleshipBoard = [];
+        this._boardDimension = boardDimension;
+        this._battleshipBoard = [];
+        this._destroyedBattleshipIds = [];
         for (let x = 0; x < boardDimension; x++) {
-            this.battleshipBoard.push(new Array(boardDimension));
+            this._battleshipBoard.push(Array(boardDimension).fill(0));
         }
     }
+    
+    public GetBoardState():number[][] {
+        return this._battleshipBoard.map(x => x.slice());
+    }
+    
+    public AddBattleShip(startX:number, startY:number, endX:number, endY:number): number {
+        if (!this.IsPointWithinBoard(startX, startY)) {
+            throw new Error(`Point (${startX}, ${startY}) is outside board.`);
+        }
+        else if (!this.IsPointWithinBoard(endX, endY)) {
+            throw new Error(`Point (${endX}, ${endY}) is outside board.`);
+        }
+        else if (!this.IsStraightLine(startX, startY, endX, endY)) {
+            throw new Error(`The position of the battleship needs to be at straight line position.`);
+        }
+        else if (!this.IsEmptyLine(startX, startY, endX, endY)) {
+            throw new Error(`Can not add a battleship from (${startX}, ${startY}) to (${endX}, ${endY}) because the space is occupied.`);
+        }
 
-    public test(): string {
-        return "Test 123";
+        let minX = Math.min(startX, endX);
+        let minY = Math.min(startY, endY);
+        let maxX = Math.max(startX, endX);
+        let maxY = Math.max(startY, endY);
+        let currentBattleshipId:number = this._newBattleshipIdCounter;
+        for (let x = minX ; x <= maxX ; x++) {
+            for (let y = minY ; y <= maxY ; y++) {
+                this._battleshipBoard[x][y] = currentBattleshipId;
+            }
+        }
+
+        this._newBattleshipIdCounter++;
+        return currentBattleshipId;
+    }
+    
+    IsStraightLine(startX:number, startY:number, endX:number, endY:number):boolean {
+        return (startX == endX) || (startY == endY);
+    }
+    
+    IsEmptyLine(startX:number, startY:number, endX:number, endY:number):boolean {
+        let minX = Math.min(startX, endX);
+        let minY = Math.min(startY, endY);
+        let maxX = Math.max(startX, endX);
+        let maxY = Math.max(startY, endY);
+        
+        for (let x = minX ; x <= maxX ; x++) {
+            for (let y = minY ; y <= maxY ; y++) {
+                if (this._battleshipBoard[x][y] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    IsPointWithinBoard(x:number, y:number):boolean {
+        if (x < 0 || this._boardDimension <= x) return false;
+        if (y < 0 || this._boardDimension <= y) return false;
+        return true;
     }
 }
--- BattleshipGame/Scripts/Tests/BattleshipBoardTest.ts
+++ BattleshipGame/Scripts/Tests/BattleshipBoardTest.ts
@@ -1,5 +1,45 @@
-﻿describe('calculate', function() {
-    it('add', function() {
-        expect(1).toBe(1);
-    })
+﻿import { BattleshipBoard } from "../BattleshipBoard";
+
+describe('BattleshipBoard.AddBattleShip tests', function() {
+    test('Adding battleship outside board should throw exception', function() {
+        expect(() => {
+            let board = new BattleshipBoard(3);
+            board.AddBattleShip(1, 1, 1, 5);
+        }).toThrowError("Point (1, 5) is outside board.")
+    });
+    test('Adding overlapping battleship is not allowed', function() {
+        expect(() => {
+            let board = new BattleshipBoard(3);
+            board.AddBattleShip(0, 0, 0, 2);
+            board.AddBattleShip(0, 1, 2, 1);
+        }).toThrowError("Can not add a battleship from (0, 1) to (2, 1) because the space is occupied.")
+    });
+    test('Adding overlapping battleship is not allowed - Reversed input line', function() {
+        expect(() => {
+            let board = new BattleshipBoard(3);
+            board.AddBattleShip(0, 0, 0, 2);
+            board.AddBattleShip(2, 1, 0, 1);
+        }).toThrowError("Can not add a battleship from (2, 1) to (0, 1) because the space is occupied.")
+    });
+    test('Adding diagonal battleship is not allowed', function() {
+        expect(() => {
+            let board = new BattleshipBoard(3);
+            board.AddBattleShip(0, 0, 2, 2);
+        }).toThrowError("The position of the battleship needs to be at straight line position.")
+    });
+    test('Successfully add battleship until the board is full', function() {
+        // We don't expect this code to throw exception
+        let board = new BattleshipBoard(3);
+        expect(board.AddBattleShip(0, 0, 0, 2)).toEqual(1); // 1
+        expect(board.AddBattleShip(1, 0, 2, 0)).toEqual(2);
+        expect(board.AddBattleShip(1, 1, 2, 1)).toEqual(3);
+        expect(board.AddBattleShip(1, 2, 1, 2)).toEqual(4);
+        expect(board.AddBattleShip(2, 2, 2, 2)).toEqual(5);
+        expect(board.GetBoardState()).toEqual([
+           [1, 1, 1],
+           [2, 3, 4],
+           [2, 3, 5] 
+        ]);
+    });
+
 });
