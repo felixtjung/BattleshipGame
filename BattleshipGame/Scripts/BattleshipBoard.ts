9417eacaeca032ab516a023605ff4b148bd01d16
Felix <felixtjung@gmail.com>
2020-11-17 17:52:58 +1100

    Add initial setup.

---
+++ BattleshipGame/Scripts/BattleshipBoard.ts
@@ -0,0 +1,14 @@
+﻿class BattleshipBoard {
+    battleshipBoard: number[][];
+
+    constructor(boardDimension: number) {
+        this.battleshipBoard = [];
+        for (let x = 0; x < boardDimension; x++) {
+            this.battleshipBoard.push(new Array(boardDimension));
+        }
+    }
+
+    public test(): string {
+        return "Test 123";
+    }
+}
