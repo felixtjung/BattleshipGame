d876db2b6f9958419ec963ed399525b142ed99e1
Felix <felixtjung@gmail.com>
2020-11-17 21:29:15 +1100

    format code.

--- BattleshipGame/Scripts/BattleshipBoard.ts
+++ BattleshipGame/Scripts/BattleshipBoard.ts
@@ -1,9 +1,25 @@
 ﻿export class BattleshipBoard {
-    private readonly _boardDimension:number;
-    private readonly _battleshipBoard:number[][];
-    private readonly _destroyedBattleshipIds:number[] = [];
-    private _totalNumberOfBattleships:number = 0;
-    private _newBattleshipIdCounter:number = 1;
+    /*
+        This class has following design constraints which must be true at all times:
+        1. The size of the _battleshipBoard multi-dimensional array are always _battleshipBoard[_boardDimension][_boardDimension]
+        2. A tile at _battleshipBoard multi-dimensional will be equal to 0 when that tile is unoccupied.
+        3. A tile at _battleshipBoard multi-dimensional will be equal to battleship-id: number when it's occupied.
+        4. When adding battleship which encompass of multiple tiles, then the occupied tiles will share the same id, for example:
+          [2, 2, 2],
+          [0, 1, 0],
+          [0, 1, 0]
+        4. When a battleship is destroyed, then battleship-id will be added on _destroyedBattleshipIds
+        5. The list of ids of _destroyedBattleshipIds must be unique
+        6. _totalNumberOfBattleships contains the number of battleship registered in this board.
+        7. The game is lost if following 2 conditions are met:
+           1. There's at least one battleship registered on this board.
+           2. _destroyedBattleshipIds.length must be equal to _totalNumberOfBattleships.length
+    */
+    private readonly _boardDimension: number;
+    private readonly _battleshipBoard: number[][];
+    private readonly _destroyedBattleshipIds: number[] = [];
+    private _totalNumberOfBattleships: number = 0;
+    private _newBattleshipIdCounter: number = 1;
 
     constructor(boardDimension: number) {
         this._boardDimension = boardDimension;
@@ -13,21 +29,39 @@
         }
     }
     
-    public GetBoardState():number[][] {
+    /**
+     * This method will return the state of the board.
+     * 
+     * Keep in mind is the value returned by this method is just a copy in order to ensure 
+     * that the board is not modified unexpectedly by outside entity.
+     */
+    public GetBoardState(): number[][] {
         return this._battleshipBoard.map(x => x.slice());
     }
-    
-    public HasLostTheGame():boolean {
-        return this._totalNumberOfBattleships > 0 && this._totalNumberOfBattleships == this._destroyedBattleshipIds.length;
+
+    /**
+     * This method will indicates whether the game has been lost or not based on 
+     * the availability of the surviving battleship.
+     */
+    public HasLostTheGame(): boolean {
+        return this._totalNumberOfBattleships > 0 
+            && this._totalNumberOfBattleships == this._destroyedBattleshipIds.length;
     }
-    
+
+    /**
+     * This method represents an attempt to attack specific location on the board.
+     * 
+     * If the attack is successful, then it will return AttackResult.Hit
+     * Otherwise, it will return AttackResult.Miss
+     * Attacking part of the battleship which has been destroyed will return AttackResult.Miss.
+     */
     public TryAttack(targetX: number, targetY: number): AttackResult {
         if (!this.IsPointWithinBoard(targetX, targetY)) {
             throw new Error(`Point (${targetX}, ${targetY}) is outside board.`);
         }
 
-        let battleshipIdOnTargetLocation:number = this._battleshipBoard[targetX][targetY];
-        let battleshipIsDestroyed = 
+        let battleshipIdOnTargetLocation: number = this._battleshipBoard[targetX][targetY];
+        let battleshipIsDestroyed: boolean = 
             // If there is battle ship at target location
             battleshipIdOnTargetLocation > 0 
             // If the battleship on target location hasn't been destroyed
@@ -39,8 +73,18 @@
 
         return AttackResult.Miss;
     }
-    
-    public AddBattleShip(startX:number, startY:number, endX:number, endY:number): number {
+
+    /**
+     * This method is called when a battleship is added to the board.
+     * 
+     * The size, location and the orientation of the battleship is defined by:
+     * (startX, startY) => (endX, endY)
+     * 
+     * When we need to add a battleship which only consists of a single tile, then 
+     * we could set (startX, startY) == (endX, endY)
+     * 
+     */
+    public AddBattleShip(startX: number, startY: number, endX: number, endY: number): number {
         if (!this.IsPointWithinBoard(startX, startY)) {
             throw new Error(`Point (${startX}, ${startY}) is outside board.`);
         }
@@ -58,7 +102,7 @@
         let minY = Math.min(startY, endY);
         let maxX = Math.max(startX, endX);
         let maxY = Math.max(startY, endY);
-        let currentBattleshipId:number = this._newBattleshipIdCounter;
+        let currentBattleshipId: number = this._newBattleshipIdCounter;
         for (let x = minX ; x <= maxX ; x++) {
             for (let y = minY ; y <= maxY ; y++) {
                 this._battleshipBoard[x][y] = currentBattleshipId;
@@ -70,11 +114,11 @@
         return currentBattleshipId;
     }
     
-    IsStraightLine(startX:number, startY:number, endX:number, endY:number):boolean {
+    IsStraightLine(startX: number, startY: number, endX: number, endY: number): boolean {
         return (startX == endX) || (startY == endY);
     }
     
-    IsEmptyLine(startX:number, startY:number, endX:number, endY:number):boolean {
+    IsEmptyLine(startX: number, startY: number, endX: number, endY: number): boolean {
         let minX = Math.min(startX, endX);
         let minY = Math.min(startY, endY);
         let maxX = Math.max(startX, endX);
@@ -87,10 +131,11 @@
                 }
             }
         }
+        
         return true;
     }
 
-    IsPointWithinBoard(x:number, y:number):boolean {
+    IsPointWithinBoard(x: number, y: number): boolean {
         if (x < 0 || this._boardDimension <= x) return false;
         if (y < 0 || this._boardDimension <= y) return false;
         return true;
